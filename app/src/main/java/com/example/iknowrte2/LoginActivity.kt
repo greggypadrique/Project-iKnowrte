a3aefdab7e123f245469e32e0d1041286356426d
Acer <greggypadrique@gmail.com>
2020-12-06 14:44:34 +0800

    Login and Signup Screen

---
+++ app/src/main/java/com/example/iknowrte2/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.iknowrte2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signupBtn.setOnClickListener {
+
+            val i = Intent(this@LoginActivity, SignupActivity::class.java)
+            startActivity(i)
+
+        }
+    }
+}
